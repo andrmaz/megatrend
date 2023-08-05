1a2314e5b83ddd281fda2b6e549040dc6db36020
andrmaz <60042277+andrmaz@users.noreply.github.com>
2023-08-05 15:22:42 +0200

    build: css

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,29 +1,29 @@
-import '../styles/404.scss'
-import { HeadFC, Link, PageProps } from 'gatsby'
-import * as React from 'react'
+import "../styles/404.scss";
+import { HeadFC, Link, PageProps } from "gatsby";
+import * as React from "react";
 
 const NotFoundPage: React.FC<PageProps> = () => {
 	return (
-		<main className='container'>
-			<h1 className='container__title'>Page not found</h1>
-			<p className='container__text'>
+		<main className="container">
+			<h1 className="container__title">Page not found</h1>
+			<p className="container__text">
 				Sorry 😔, we couldn’t find what you were looking for.
 				<br />
-				{process.env.NODE_ENV === 'development' ? (
+				{process.env.NODE_ENV === "development" ? (
 					<>
 						<br />
-						Try creating a page in{' '}
-						<code className='container__code'>src/pages/</code>.
+						Try creating a page in{" "}
+						<code className="container__code">src/pages/</code>.
 						<br />
 					</>
 				) : null}
 				<br />
-				<Link to='/'>Go home</Link>.
+				<Link to="/">Go home</Link>.
 			</p>
 		</main>
-	)
-}
+	);
+};
 
-export default NotFoundPage
+export default NotFoundPage;
 
-export const Head: HeadFC = () => <title>Not found</title>
+export const Head: HeadFC = () => <title>Not found</title>;
