a222b66ed4ae19b4ac6e6d73463a590a8b370409
andrmaz <60042277+andrmaz@users.noreply.github.com>
2023-09-23 15:59:22 +0200

    styles: linaria

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,19 +1,20 @@
-import "../styles/404.scss";
+import { css } from '@linaria/core';
+import {tokens} from '@shopify/polaris-tokens';
 import { HeadFC, Link, PageProps } from "gatsby";
 import * as React from "react";
 
 const NotFoundPage: React.FC<PageProps> = () => {
 	return (
-		<main className="container">
-			<h1 className="container__title">Page not found</h1>
-			<p className="container__text">
+		<main className={container}>
+			<h1 className={title}>Page not found</h1>
+			<p className={text}>
 				Sorry 😔, we couldn’t find what you were looking for.
 				<br />
 				{process.env.NODE_ENV === "development" ? (
 					<>
 						<br />
 						Try creating a page in{" "}
-						<code className="container__code">src/pages/</code>.
+						<code className={code}>src/pages/</code>.
 						<br />
 					</>
 				) : null}
@@ -23,6 +24,24 @@ const NotFoundPage: React.FC<PageProps> = () => {
 		</main>
 	);
 };
+
+const container = css`
+	padding: ${tokens.space['space-2']};
+`;
+const title = css`
+	margin-top: ${tokens.space['space-0']};
+    margin-bottom: ${tokens.space['space-10']};
+`;
+const text = css`
+	margin-bottom: ${tokens.space['space-8']};
+`;
+const code = css`
+	color: ${tokens.color['color-text-info']};
+	padding: ${tokens.space['space-05']};
+	background-color: ${tokens.color['color-bg-info-strong']};
+	font-size: ${tokens.font['font-size-200']};
+	border-radius: ${tokens.border['border-radius-1']};
+`;
 
 export default NotFoundPage;
 
