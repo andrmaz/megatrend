bf2521f78c7b5fcf26b7b94abcdecd5faed4fbda
andrmaz <60042277+andrmaz@users.noreply.github.com>
2023-11-02 23:41:22 +0100

    styles: filings

--- src/templates/index.tsx
+++ src/templates/index.tsx
@@ -1,5 +1,6 @@
 import "../styles/globals";
 import { TrendingStock } from "../types/trending-stocks";
+import { path } from "../utils/constants";
 import { formatStringDate } from "../utils/date";
 import { css } from "@linaria/core";
 import { tokens } from "@shopify/polaris-tokens";
@@ -13,20 +14,26 @@ const IndexPage = (props: Props) => {
 		<div className={wrapper}>
 			<header className={header}>
 				<h1 className={title}>Megatrend</h1>
+				<nav className={navigation}>
+					<a href={path.filings} className={link}>
+						Late Filings
+					</a>
+				</nav>
 			</header>
 			<main className={container}>
 				<h2 className={subtitle}>Trending Stocks</h2>
 				<div className={content}>
-					{props.pageContext.trendingStocks.map((stock) => {
+					{props.pageContext.trendingStocks.map((stock, index) => {
 						return (
-							<article key={stock.Identifier} className={card}>
+							<article key={index + stock.Identifier} className={card}>
 								<div className={heading}>
 									<hgroup>
-										<h3 className={identifier}>{stock.Identifier}</h3>
+										<h3 className={ticker}>{stock.Identifier}</h3>
 										<p className={company}>{stock.Company}</p>
 									</hgroup>
 									<time>{formatStringDate(stock.FilingDate)}</time>
 								</div>
+								<span>Score: </span>
 								<b>{stock.Score}</b>
 							</article>
 						);
@@ -42,8 +49,17 @@ const wrapper = css`
 `;
 const header = css`
 	background-color: ${tokens.color["color-bg-primary-active"]};
+	display: flex;
 	padding: ${tokens.space["space-10"]};
 	width: 100%;
+`;
+const navigation = css`
+	margin-left: auto;
+`;
+const link = css`
+	color: ${tokens.color["color-text-inverse"]};
+	font-size: ${tokens.font["font-line-height-3"]};
+	text-decoration: none;
 `;
 const container = css`
 	padding: ${tokens.space["space-10"]};
@@ -64,19 +80,23 @@ const subtitle = css`
 	font-size: ${tokens.font["font-size-500"]};
 `;
 const card = css`
+	background-color: ${tokens.color["color-bg-secondary-experimental"]};
 	border-radius: ${tokens.border["border-radius-5"]};
 	border: ${tokens.border["border-width-4"]} solid ${tokens.color["color-border"]};
 	min-width: 300px;
 	padding: ${tokens.space["space-4"]};
 `;
 const heading = css`
+	align-items: baseline;
 	display: flex;
 	justify-content: space-between;
+	margin-bottom: ${tokens.space["space-4"]};
 `;
-const identifier = css`
+const ticker = css`
 	margin-top: 0;
 `;
 const company = css`
+	margin-top: 0;
     width: 200px;
     text-overflow: ellipsis;
     white-space: nowrap;
